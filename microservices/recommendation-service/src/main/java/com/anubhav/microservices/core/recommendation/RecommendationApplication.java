731cbf63a0504164c8d737ca43d2c6845d002327
anubhav1011 <gupta.anu@husky.neu.edu>
2021-01-01 23:44:30 -0500

    Creating Recommendation microservice

--- microservices/recommendation-service/src/main/java/com/anubhav/microservices/core/recommendation/RecommendationApplication.java
+++ microservices/recommendation-service/src/main/java/com/anubhav/microservices/core/recommendation/RecommendationApplication.java
@@ -2,8 +2,11 @@ package com.anubhav.microservices.core.recommendation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@ComponentScan("com.anubhav.microservices")
 public class RecommendationApplication {
 
 	public static void main(String[] args) {
